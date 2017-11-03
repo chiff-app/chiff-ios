eaeb198d6336d8166f08397bb93bb69ceb51c86e
bas-d <7903735+bas-d@users.noreply.github.com>
2017-11-03 15:01:18 +0100

    UI initial commit

---
+++ athena/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  athena
+//
+//  Created by bas on 03/11/2017.
+//  Copyright © 2017 athena. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
