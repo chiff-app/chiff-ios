deb7bbb9846b179154ccc0ab45829cde072af4b3
bas-d <7903735+bas-d@users.noreply.github.com>
2018-02-02 15:36:06 +0100

    Improved authentication logic for logging in and login requests.

---
+++ keyn/Controllers/Authentication/AuthenticationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AuthenticationViewController.swift
+//  keyn
+//
+//  Created by bas on 02/02/2018.
+//  Copyright © 2018 keyn. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ keyn/Controllers/Authentication/LoginViewController.swift
@@ -0,0 +1,166 @@
+//
+//  LoginViewController.swift
+//  keyn
+//
+//  Created by Bas Doorn on 09/12/2017.
+//  Copyright © 2017 keyn. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+import SmileLock
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var touchIdButton: UIButton!
+    var autoAuthentication = true
+    
+    override func viewDidLoad() {
+        print("Login view controller loaded")
+        super.viewDidLoad()
+        touchIdButton.imageView!.contentMode = .scaleAspectFit
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Login view controller appeared")
+        if autoAuthentication {
+            authenticateUser()
+        }
+    }
+
+//    func authenticateUser() {
+//        let authenticationContext = LAContext()
+//        var error: NSError?
+//
+//        guard authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+//            print("Todo: handle fingerprint absence \(String(describing: error))")
+//            return
+//        }
+//
+//        authenticationContext.evaluatePolicy(
+//            .deviceOwnerAuthenticationWithBiometrics,
+//            localizedReason: "Unlock Keyn",
+//            reply: { [weak self] (success, error) -> Void in
+//                if (success) {
+//                    DispatchQueue.main.async {
+//                        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+//                            appDelegate.authenticated = true
+//                        }
+//                        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//                        let viewController = storyboard.instantiateViewController(withIdentifier: "RootController") as! RootViewController
+//                        UIApplication.shared.keyWindow?.rootViewController = viewController
+//                    }
+//                } else {
+//                    print("Todo")
+//                }
+//            }
+//        )
+//    }
+
+    @IBAction func authenticateButton(_ sender: UIButton) {
+        authenticateUser()
+    }
+}
+
+extension LoginViewController {
+
+    func authenticateUser() {
+
+        let localAuthenticationContext = LAContext()
+        localAuthenticationContext.localizedFallbackTitle = "Use Passcode"
+
+        var authError: NSError?
+        let reasonString = "Unlock Keyn"
+
+        guard localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) else {
+            //TODO: Show appropriate alert if biometry/TouchID/FaceID is lockout or not enrolled
+            print(self.evaluateAuthenticationPolicyMessageForLA(errorCode: authError!.code))
+            return
+        }
+
+        localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString) { success, evaluateError in
+
+            if success {
+                DispatchQueue.main.async {
+                    let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let viewController = storyboard.instantiateViewController(withIdentifier: "RootController") as! RootViewController
+                    UIApplication.shared.keyWindow?.rootViewController = viewController
+                }
+            } else {
+                //TODO: User did not authenticate successfully, look at error and take appropriate action
+                guard let error = evaluateError else {
+                    return
+                }
+
+                print(self.evaluateAuthenticationPolicyMessageForLA(errorCode: error._code))
+                //TODO: If you have choosen the 'Fallback authentication mechanism selected' (LAError.userFallback). Handle gracefully
+                if error._code == LAError.userFallback.rawValue {
+                    print("Handle fallback")
+                }
+
+            }
+        }
+    }
+
+
+    func evaluatePolicyFailErrorMessageForLA(errorCode: Int) -> String {
+        var message = ""
+        if #available(iOS 11.0, *) {
+            switch errorCode {
+            case LAError.biometryNotAvailable.rawValue:
+                message = "Authentication could not start because the device does not support biometric authentication."
+            case LAError.biometryLockout.rawValue:
+                message = "Authentication could not continue because the user has been locked out of biometric authentication, due to failing authentication too many times."
+            case LAError.biometryNotEnrolled.rawValue:
+                message = "Authentication could not start because the user has not enrolled in biometric authentication."
+            default:
+                message = "Did not find error code on LAError object"
+            }
+        } else {
+            switch errorCode {
+            case LAError.touchIDLockout.rawValue:
+                message = "Too many failed attempts."
+            case LAError.touchIDNotAvailable.rawValue:
+                message = "TouchID is not available on the device"
+            case LAError.touchIDNotEnrolled.rawValue:
+                message = "TouchID is not enrolled on the device"
+            default:
+                message = "Did not find error code on LAError object"
+            }
+        }
+
+        return message;
+    }
+
+
+    func evaluateAuthenticationPolicyMessageForLA(errorCode: Int) -> String {
+
+        var message = ""
+
+        switch errorCode {
+        case LAError.authenticationFailed.rawValue:
+            message = "The user failed to provide valid credentials"
+        case LAError.appCancel.rawValue:
+            message = "Authentication was cancelled by application"
+        case LAError.invalidContext.rawValue:
+            message = "The context is invalid"
+        case LAError.notInteractive.rawValue:
+            message = "Not interactive"
+        case LAError.passcodeNotSet.rawValue:
+            message = "Passcode is not set on the device"
+        case LAError.systemCancel.rawValue:
+            message = "Authentication was cancelled by the system"
+        case LAError.userCancel.rawValue:
+            message = "The user did cancel"
+        case LAError.userFallback.rawValue:
+            message = "The user chose to use the fallback"
+        default:
+            message = evaluatePolicyFailErrorMessageForLA(errorCode: errorCode)
+        }
+
+        return message
+    }
+}
