f72e8387fc0b3d7b0c3416d31b2c08709724ab2f
sourcx <25772871+sourcx@users.noreply.github.com>
2018-02-06 22:06:30 +0100

    Moved parsing/decrypting of notifications to testable file.

---
+++ keyn/NotificationServiceHelper.swift
@@ -0,0 +1,33 @@
+import UserNotifications
+
+class NotificationServiceHelper {
+
+    static func decrypt(_ content: UNMutableNotificationContent?) -> UNMutableNotificationContent? {
+        guard let content = content else {
+            return nil
+        }
+
+        if let ciphertext = content.userInfo["data"] as? String, let id = content.userInfo["sessionID"] as? String {
+            do {
+                if let session = try Session.getSession(id: id) {
+                    let message = try session.decrypt(message: ciphertext)
+                    let parts = message.split(separator: " ")
+                    let siteID = parts[0]
+                    let browserTab = parts[1]
+
+                    let site = Site.get(id: String(siteID))
+                    content.body = "Login request for \(site.name) from \(session.browser) on \(session.os)."
+                    content.userInfo["siteID"] = siteID
+                    content.userInfo["browserTab"] = browserTab
+
+                    return content
+                }
+            } catch {
+                print("Session could not be decoded: \(error)")
+            }
+        }
+
+        return nil
+    }
+
+}
--- keynNotificationExtension/NotificationService.swift
+++ keynNotificationExtension/NotificationService.swift
@@ -1,48 +1,25 @@
-//
-//  NotificationService.swift
-//  keynNotificationExtension
-//
-//  Created by bas on 19/01/2018.
-//  Copyright © 2018 keyn. All rights reserved.
-//
-
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
+    var content: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        if let bestAttemptContent = bestAttemptContent {
-            if let ciphertext = bestAttemptContent.userInfo["data"] as? String, let id = bestAttemptContent.userInfo["sessionID"] as? String {
-                do {
-                    if let session = try Session.getSession(id: id) {
-                        let message = try session.decrypt(message: ciphertext)
-                        let parts = message.split(separator: " ")
-                        let siteID = parts[0]
-                        let browserTab = parts[1]
+        content = (request.content.mutableCopy() as? UNMutableNotificationContent)
 
-                        let site = Site.get(id: String(siteID))
-                        bestAttemptContent.body = "Login request for \(site.name) from \(session.browser) on \(session.os)."
-                        bestAttemptContent.userInfo["siteID"] = siteID
-                        bestAttemptContent.userInfo["browserTab"] = browserTab
-                    }
-                } catch {
-                    print("Session could not be decoded: \(error)")
-                }
-            }
-            contentHandler(bestAttemptContent)
+        if let content = NotificationServiceHelper.decrypt(content) {
+            contentHandler(content)
         }
     }
 
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content,
+    // otherwise the original push payload will be used.
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
+        if let contentHandler = contentHandler, let content =  content {
+            contentHandler(content)
         }
     }
 
