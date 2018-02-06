fa62d72c3ac6224cd763bd9dc39f1cd21f2fe59e
sourcx <25772871+sourcx@users.noreply.github.com>
2018-02-06 19:24:15 +0100

    Fix Site test.

--- keynTests/SiteTests.swift
+++ keynTests/SiteTests.swift
@@ -2,19 +2,18 @@ import XCTest
 @testable import keyn
 
 class SiteTests: XCTestCase {
+    let restrictions = PasswordRestrictions(length: 24, characters: [.lower, .numbers, .upper, .symbols])
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
     func testInitAssignsURL() {
-        let site = Site(name : "", id: "GOOGLE", urls: ["google.com", "accounts.google.com"])
+        let site = Site(name : "Google", id: "0", urls: ["google.com", "accounts.google.com"], restrictions: restrictions)
         XCTAssertEqual(site.urls, ["google.com", "accounts.google.com"])
     }
 
