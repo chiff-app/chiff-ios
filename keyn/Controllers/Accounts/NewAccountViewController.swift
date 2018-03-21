98a1000658aa8b8a147e5e495578bf2aeed8cc6c
bas-d <7903735+bas-d@users.noreply.github.com>
2018-03-21 17:36:02 +0100

    Password validation refactoring; password validation for custom passwords

--- keyn/Controllers/Accounts/NewAccountViewController.swift
+++ keyn/Controllers/Accounts/NewAccountViewController.swift
@@ -6,8 +6,14 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
     
     var passwordIsHidden = true
     var customPassword = false
+    let ppd: PPD? = nil
+    var passwordValidator: PasswordValidator? = nil
     @IBOutlet weak var saveButton: UIBarButtonItem!
-    
+    @IBOutlet weak var requirementsView: UIView!
+    @IBOutlet var requirementLabels: [UILabel]!
+    @IBOutlet weak var requirementLabelsStackView: UIStackView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -15,6 +21,12 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
             textField?.delegate = self
             textField?.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         }
+
+        requirementLabels.sort(by: { (first, second) -> Bool in
+            return first.tag < second.tag
+        })
+
+        // TODO: get PPD?
         
         updateSaveButtonState()
 
@@ -72,6 +84,11 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
             customPassword = true
             userPasswordTextField.isEnabled = true
             userPasswordTextField.text = ""
+            if passwordValidator == nil {
+                passwordValidator = PasswordValidator(ppd: ppd)
+            }
+            requirementsView.isHidden = false
+            updatePasswordRequirements(password: userPasswordTextField.text ?? "")
             tableView.reloadRows(at: [passwordRowIndex], with: .bottom)
             tableView.reloadSections([1], with: .fade)
             tableView.reloadData()
@@ -80,6 +97,7 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
             tableView.reloadRows(at: [passwordRowIndex], with: .top)
             tableView.reloadSections([1], with: .fade)
             tableView.reloadData()
+            requirementsView.isHidden = true
             userPasswordTextField.isEnabled = false
             updateSaveButtonState()
         }
@@ -87,6 +105,7 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
     
     
     // MARK: Private Methods
+
     
     // Disable the Save button if one the text fields is empty.
     private func updateSaveButtonState() {
@@ -95,10 +114,36 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
         let userName = userNameTextField.text ?? ""
         let password = userPasswordTextField.text ?? ""
 
-        if (websiteName.isEmpty || websiteURL.isEmpty || userName.isEmpty || (customPassword && password.isEmpty)) {
+        updatePasswordRequirements(password: password)
+
+        if (websiteName.isEmpty || websiteURL.isEmpty || userName.isEmpty || !isValidPassword(password: password)) {
             saveButton.isEnabled = false
         } else {
             saveButton.isEnabled = true
+        }
+    }
+
+    private func isValidPassword(password: String) -> Bool {
+        if customPassword {
+            if password.isEmpty { return false }
+            if let passwordValidator = passwordValidator {
+                return passwordValidator.validate(password: password)
+            }
+        }
+        return true
+    }
+
+    // TODO: Fetch correct requirements from PPD and present to user.
+    private func updatePasswordRequirements(password: String) {
+        if let passwordValidator = passwordValidator {
+            requirementLabels[0].text = passwordValidator.validateMinLength(password: password) ? "" : "\u{26A0} The password needs to be at least \(8) characters."
+            requirementLabels[1].text = passwordValidator.validateMaxLength(password: password) ? "" : "\u{26A0} The password can have no more than \(50) characters."
+            requirementLabels[2].text = passwordValidator.validateCharacters(password: password) ? "" : "\u{26A0} The password has invalid characters."
+            requirementLabels[3].text = passwordValidator.validateCharacterSet(password: password) ? "" : "\u{26A0} CharacterSet constraint"
+            requirementLabels[4].text = passwordValidator.validateConsecutiveCharacters(password: password) ? "" : "\u{26A0} The password can't have more than \(3) consecutive characters like aaa or ***."
+            requirementLabels[5].text = passwordValidator.validatePositionRestrictions(password: password) ? "" : "\u{26A0} The password needs to start with a.. "
+            requirementLabels[6].text = passwordValidator.validateRequirementGroups(password: password) ? "" : "\u{26A0} Pff..."
+            requirementLabels[7].text = passwordValidator.validateConsecutiveOrderedCharacters(password: password) ? "" : "\u{26A0} The password can't have consecutive characters like abc pr 0123."
         }
     }
     
@@ -112,7 +157,6 @@ class NewAccountViewController: AccountViewController, UITextFieldDelegate {
             print("TODO: Site info + id should be fetched from somewhere instead of generated here..")
 
             let id = String(websiteName + websiteURL).hashValue
-            let restrictions = PasswordRestrictions(length: 24, characters: [.lower, .numbers, .upper, .symbols])
             let site = Site(name: websiteName, id: id, urls: [websiteURL], ppd: nil)
 
             do {
