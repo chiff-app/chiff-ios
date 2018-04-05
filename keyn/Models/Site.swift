6aa444f49506018b7286719310f982428aecffef
bas-d <7903735+bas-d@users.noreply.github.com>
2018-04-05 10:16:47 +0200

    Site now also gets added when a change password request is received for an unknown site.

--- keyn/Models/Site.swift
+++ keyn/Models/Site.swift
@@ -18,7 +18,7 @@ struct Site: Codable {
         sampleSites.append(Site(name: "LinkedIn", id: 0, urls: ["https://www.linkedin.com"], ppd: getSamplePPD(id: id)))
         sampleSites.append(Site(name: "Gmail", id: 1, urls: ["google.com", "accounts.google.com"], ppd: getSamplePPD(id: id)))
         sampleSites.append(Site(name: "ProtonMail", id: 2, urls: ["https://mail.protonmail.com/login"], ppd: getSamplePPD(id: id)))
-        sampleSites.append(Site(name: "University of London", id: 3, urls: ["https://my.londoninternational.ac.uk/login"], ppd: getSamplePPD(id: id)))
+        sampleSites.append(Site(name: "Twitter", id: 3, urls: ["https://twitter.com/login"], ppd: getSamplePPD(id: id)))
         sampleSites.append(Site(name: "Github", id: 4, urls: ["https://github.com/login"], ppd: getSamplePPD(id: id)))
         sampleSites.append(Site(name: "DigitalOcean", id: 5, urls: ["https://digitalocean.com/login"], ppd: getSamplePPD(id: id)))
         sampleSites.append(Site(name: "DigitalOcean", id: 6, urls: ["https://complicatedExample.com"], ppd: getSamplePPD(id: id)))
