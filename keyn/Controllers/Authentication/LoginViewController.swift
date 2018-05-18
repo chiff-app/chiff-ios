31b6c0735bd88280fe8538ee1afde46c7d30c80f
bas-d <7903735+bas-d@users.noreply.github.com>
2018-05-18 16:11:51 +0200

    Added power mode.

--- keyn/Controllers/Authentication/LoginViewController.swift
+++ keyn/Controllers/Authentication/LoginViewController.swift
@@ -13,6 +13,8 @@ import os.log
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var touchIDButton: UIButton!
+    @IBOutlet weak var powerModeSwitch: UISwitch!
+    var originalBrightness: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +31,26 @@ class LoginViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
     }
+    
+    private func togglePowerMode(on: Bool) {
+        UIApplication.shared.isIdleTimerDisabled = on
+        if originalBrightness == nil {
+            originalBrightness = UIScreen.main.brightness
+        }
+        UIScreen.main.brightness = on ? min(originalBrightness ?? 1.0, 0.1) : originalBrightness ?? 0.5
+    }
 
     // MARK: Actions
     @IBAction func touchID(_ sender: UIButton) {
         os_log("Manual authenticate in viewDidAppear called", type: .debug)
+        powerModeSwitch.setOn(false, animated: true)
+        togglePowerMode(on: false)
         AuthenticationGuard.sharedInstance.authenticateUser(cancelChecks: false)
     }
 
     @IBAction func unwindToLoginViewController(sender: UIStoryboardSegue) { }
+    
+    @IBAction func powerModeAction(_ sender: UISwitch) {
+        togglePowerMode(on: sender.isOn)
+    }
 }
