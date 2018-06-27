3d1c855b15c05ca003c9421f770669464ff4217a
bas-d <7903735+bas-d@users.noreply.github.com>
2018-06-27 14:18:56 +0200

    Updated to work with new Sodium version

--- keyn/Crypto/Crypto.swift
+++ keyn/Crypto/Crypto.swift
@@ -52,7 +52,7 @@ class Crypto {
 
     func deriveKeyFromSeed(seed: Data, keyType: KeyType, context: String) throws -> Data {
         // This expands the 128-bit seed to 256 bits by hashing. Necessary for key derivation.
-        guard let seedHash = sodium.genericHash.hash(message: seed) else {
+        guard let seedHash = sodium.genericHash.hash(message: seed.bytes) else {
             throw CryptoError.hashing
         }
         
@@ -61,7 +61,7 @@ class Crypto {
             throw CryptoError.keyDerivation
         }
 
-        return key
+        return key.data
     }
 
 
@@ -73,34 +73,34 @@ class Crypto {
         return keyPair
     }
     
-    func createBackupKeyPair(seed: Data) throws -> Sign.KeyPair {
-        guard let keyPair = sodium.sign.keyPair(seed: seed) else {
+    func createBackupKeyPair(seed: Data) throws -> Box.KeyPair {
+        guard let keyPair = sodium.sign.keyPair(seed: seed.bytes) else {
             throw CryptoError.keyGeneration
         }
         return keyPair
     }
 
     func deterministicRandomBytes(seed: Data, length: Int) throws -> Data {
-        guard let keyData = sodium.randomBytes.deterministic(length: length, seed: seed) else {
+        guard let keyData = sodium.randomBytes.deterministic(length: length, seed: seed.bytes) else {
             throw CryptoError.keyDerivation
         }
         
-        return keyData
+        return keyData.data
     }
 
 
     func deriveKey(keyData: Data, context: Data, index: Int = 0, keyLengthBytes: Int = 32) throws ->  Data {
-        guard let contextHash = sodium.genericHash.hash(message: context, outputLength: 8) else {
+        guard let contextHash = sodium.genericHash.hash(message: context.bytes, outputLength: 8) else {
             throw CryptoError.hashing
         }
         guard let context = sodium.utils.bin2base64(contextHash, variant: .ORIGINAL_NO_PADDING) else {
             throw CryptoError.base64Encoding
         }
-        guard let key = sodium.keyDerivation.derive(secretKey: keyData, index: UInt64(index), length: keyLengthBytes, context: String(context.prefix(8))) else {
+        guard let key = sodium.keyDerivation.derive(secretKey: keyData.bytes, index: UInt64(index), length: keyLengthBytes, context: String(context.prefix(8))) else {
             throw CryptoError.keyDerivation
         }
 
-        return key
+        return key.data
     }
 
 
@@ -109,16 +109,16 @@ class Crypto {
 
     func convertFromBase64(from base64String: String) throws -> Data  {
         // Convert from base64 to Data
-        guard let data = sodium.utils.base642bin(base64String, variant: .URLSAFE_NO_PADDING, ignore: nil) else {
+        guard let bytes = sodium.utils.base642bin(base64String, variant: .URLSAFE_NO_PADDING, ignore: nil) else {
             throw CryptoError.base64Decoding
         }
 
-        return data
+        return bytes.data
     }
 
     func convertToBase64(from data: Data) throws -> String  {
         // Convert from Data to base64
-        guard let b64String = sodium.utils.bin2base64(data, variant: .URLSAFE_NO_PADDING) else {
+        guard let b64String = sodium.utils.bin2base64(data.bytes, variant: .URLSAFE_NO_PADDING) else {
             throw CryptoError.base64Encoding
         }
 
@@ -127,66 +127,66 @@ class Crypto {
     
     // MARK: Signing functions
     
-    func sign(message: Data, privKey: Sign.SecretKey) throws -> Data {
-        guard let signedMessage = sodium.sign.sign(message: message, secretKey: privKey) else {
+    func sign(message: Data, privKey: Data) throws -> Data {
+        guard let signedMessage = sodium.sign.sign(message: message.bytes, secretKey: privKey.bytes) else {
             throw CryptoError.signing
         }
         
-        return signedMessage
+        return signedMessage.data
     }
 
 
     // MARK: Encryption & decryption functions
     
     func encryptSymmetric(_ plaintext: Data, secretKey: Data) throws -> Data {
-        guard let ciphertext: Data = sodium.secretBox.seal(message: plaintext, secretKey: secretKey) else {
+        guard let ciphertext: Bytes = sodium.secretBox.seal(message: plaintext.bytes, secretKey: secretKey.bytes) else {
             throw CryptoError.encryption
         }
         
-        return ciphertext
+        return ciphertext.data
     }
     
     func decryptSymmetric(_ ciphertext: Data, secretKey: Data) throws -> Data {
-        guard let plaintext: Data = sodium.secretBox.open(nonceAndAuthenticatedCipherText: ciphertext, secretKey: secretKey) else {
+        guard let plaintext: Bytes = sodium.secretBox.open(nonceAndAuthenticatedCipherText: ciphertext.bytes, secretKey: secretKey.bytes) else {
             throw CryptoError.encryption
         }
         
-        return plaintext
+        return plaintext.data
     }
 
-    func encrypt(_ plaintext: Data, pubKey: Box.PublicKey, privKey: Box.SecretKey) throws -> Data {
-        guard let ciphertext: Data = sodium.box.seal(message: plaintext, recipientPublicKey: pubKey, senderSecretKey: privKey) else {
+    func encrypt(_ plaintext: Data, pubKey: Data, privKey: Data) throws -> Data {
+        guard let ciphertext: Bytes = sodium.box.seal(message: plaintext.bytes, recipientPublicKey: pubKey.bytes, senderSecretKey: privKey.bytes) else {
             throw CryptoError.encryption
         }
     
-        return ciphertext
+        return ciphertext.data
     }
 
-    func encrypt(_ plaintext: Data, pubKey: Box.PublicKey) throws -> Data {
-        guard let ciphertext: Data = sodium.box.seal(message: plaintext, recipientPublicKey: pubKey) else {
+    func encrypt(_ plaintext: Data, pubKey: Data) throws -> Data {
+        guard let ciphertext: Bytes = sodium.box.seal(message: plaintext.bytes, recipientPublicKey: pubKey.bytes) else {
             throw CryptoError.encryption
         }
-        return ciphertext
+        return ciphertext.data
     }
 
     // This function should decrypt a password request with the sessions corresponding session / private key and check signature with browser's public key
-    func decrypt(_ ciphertext: Data, privKey: Box.SecretKey, pubKey: Box.PublicKey) throws -> (Data, Data) {
+    func decrypt(_ ciphertext: Data, privKey: Data, pubKey: Data) throws -> (Data, Data) {
         let nonce = ciphertext[..<Data.Index(sodium.box.NonceBytes)]
-        guard let plaintext: Data = sodium.box.open(nonceAndAuthenticatedCipherText: ciphertext, senderPublicKey: pubKey, recipientSecretKey: privKey) else {
+        guard let plaintext: Bytes = sodium.box.open(nonceAndAuthenticatedCipherText: ciphertext.bytes, senderPublicKey: pubKey.bytes, recipientSecretKey: privKey.bytes) else {
             throw CryptoError.decryption
         }
         
-        return (plaintext, nonce)
+        return (plaintext.data, nonce)
     }
 
 
      // MARK: Hash functions
 
     func hash(_ data: Data) throws -> Data {
-        guard let hashData = sodium.genericHash.hash(message: data) else {
+        guard let hashData = sodium.genericHash.hash(message: data.bytes) else {
             throw CryptoError.hashing
         }
-        return hashData
+        return hashData.data
     }
 
     func hash(_ message: String) throws -> String {
@@ -194,7 +194,7 @@ class Crypto {
             throw CryptoError.convertToData
         }
         let hashData = try hash(messageData)
-        guard let hash = sodium.utils.bin2hex(hashData) else {
+        guard let hash = sodium.utils.bin2hex(hashData.bytes) else {
             throw CryptoError.convertToHex
         }
         return hash
