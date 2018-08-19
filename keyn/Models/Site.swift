6a976b073f7269e21ab8d5325aa4c3d761c20b20
bas-d <7903735+bas-d@users.noreply.github.com>
2018-08-19 02:40:21 +0200

    Should work with new API. Needs testing.

--- keyn/Models/Site.swift
+++ keyn/Models/Site.swift
@@ -8,14 +8,8 @@ struct Site: Codable {
     var ppd: PPD?
 
     static func get(id: String, completion: @escaping (_ site: Site) -> Void) {
-        if UserDefaults.standard.bool(forKey: "ppdTestingMode") {
-            AWS.sharedInstance.getDevelopmentPPD(id: id) { (ppd) in
-                completion(Site(name: ppd.name ?? "Unknown", id: id, url: ppd.url, ppd: ppd))
-            }
-        } else {
-            AWS.sharedInstance.getPPD(id: id) { (ppd) in
-                completion(Site(name: ppd.name ?? "Unknown", id: id, url: ppd.url, ppd: ppd))
-            }
+        PPD.get(id: id) { (ppd) in
+            completion(Site(name: ppd.name ?? "Unknown", id: id, url: ppd.url, ppd: ppd))
         }
     }
 
