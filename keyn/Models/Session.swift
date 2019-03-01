9faf8c8e964a6e08d0e4bab4ec72f59be139212c
bas-d <7903735+bas-d@users.noreply.github.com>
2019-03-01 18:53:39 +0100

    Removed AccountList messageType

--- keyn/Models/Session.swift
+++ keyn/Models/Session.swift
@@ -306,7 +306,7 @@ class Session: Codable {
     }
 
     private func sendByeToPersistentQueue(completionHandler: @escaping (_ res: [String: Any]?, _ error: Error?) -> Void) throws {
-        let message = try JSONEncoder().encode(KeynPersistentQueueMessage(accounts: nil, passwordSuccessfullyChanged: nil, accountID: nil, type: .end, receiptHandle: nil))
+        let message = try JSONEncoder().encode(KeynPersistentQueueMessage(passwordSuccessfullyChanged: nil, accountID: nil, type: .end, receiptHandle: nil))
         let ciphertext = try Crypto.shared.encrypt(message, key: sharedKey())
         apiRequest(endpoint: .persistent, method: .post, message: ["data": ciphertext.base64]) { (_, error) in
             if let error = error {
