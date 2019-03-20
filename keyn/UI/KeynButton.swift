b3df064ae10717a9e97a6c1f8c5e4a34b24ab0a8
bas-d <7903735+bas-d@users.noreply.github.com>
2019-03-20 16:48:04 +0100

    How Keyn Works page

--- keyn/UI/KeynButton.swift
+++ keyn/UI/KeynButton.swift
@@ -58,4 +58,46 @@ enum KeynButtonType: String {
 
 }
 
+@IBDesignable class KeynTabButton: UIButton {
+
+    @IBInspectable var keynButtonType: String {
+        get {
+            return self.type.rawValue
+        }
+        set(type) {
+            self.type = KeynButtonType(rawValue: type) ?? .primary
+        }
+    }
+
+    var type: KeynButtonType = .primary {
+        didSet {
+            if type == .primary {
+                layer.maskedCorners = [.layerMaxXMinYCorner]
+            } else {
+                layer.maskedCorners = [.layerMinXMinYCorner]
+            }
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+
+    func sharedInit() {
+        layer.cornerRadius = frame.size.height / 2
+        titleLabel?.font = UIFont(name: "Montserrat-Bold", size: 14.0)
+    }
+
+}
+
 
