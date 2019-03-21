ce66ed66f534441499ef4508e7328f8f0c035f7d
bas-d <7903735+bas-d@users.noreply.github.com>
2019-03-21 16:45:52 +0100

    Login + Request views

--- keyn/Controllers/Authentication/LoginViewController.swift
+++ keyn/Controllers/Authentication/LoginViewController.swift
@@ -7,21 +7,6 @@ import LocalAuthentication
 
 class LoginViewController: UIViewController {
     
-    private let inset: CGFloat = 13
-    
-    @IBOutlet weak var touchIDButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        touchIDButton.imageView!.contentMode = .scaleAspectFit
-        touchIDButton.imageEdgeInsets = UIEdgeInsets.init(top: inset, left: inset, bottom: inset, right: inset)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
     }
