25e8fd62fa44f275f1b5a89d50b01f6dfd46f450
bas-d <7903735+bas-d@users.noreply.github.com>
2019-04-15 17:52:39 +0200

    Backslash fix

--- keyn/Core/PasswordValidator.swift
+++ keyn/Core/PasswordValidator.swift
@@ -10,7 +10,7 @@ class PasswordValidator {
     static let MIN_PASSWORD_LENGTH_BOUND = 8
     static let MAX_PASSWORD_LENGTH_BOUND = 50
     static let OPTIMAL_CHARACTER_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321"
-    static let MAXIMAL_CHARACTER_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321)(*&^%$#@!{}[]:;\"'?/,.<>`~|-_+= "
+    static let MAXIMAL_CHARACTER_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321)(*&^%$#@!{}[]:;\"'?/,.<>`~|-_+= \\"
     let ppd: PPD?
     var characterSetDictionary = [String:String]()
     var characters = ""
