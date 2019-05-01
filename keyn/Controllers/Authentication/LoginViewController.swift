ee68d7811eb82c388ea3af9706d97d63cf3ebac9
bas-d <7903735+bas-d@users.noreply.github.com>
2019-05-01 11:17:35 +0200

    Present error if data is corrupted

--- keyn/Controllers/Authentication/LoginViewController.swift
+++ keyn/Controllers/Authentication/LoginViewController.swift
@@ -16,4 +16,26 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func unwindToLoginViewController(sender: UIStoryboardSegue) { }
+
+    func showDecodingError(error: DecodingError) {
+        let alert = UIAlertController(title: "errors.corrupted_data".localized, message: "popups.questions.delete_corrupted".localized, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "popups.responses.cancel".localized, style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "popups.responses.delete".localized, style: .destructive, handler: { action in
+            do {
+                Session.deleteAll()
+                Account.deleteAll()
+                try Seed.delete()
+                BackupManager.shared.deleteEndpoint()
+                BackupManager.shared.deleteAllKeys()
+                Logger.shared.warning("Keyn reset after corrupted data", error: error)
+                let storyboard: UIStoryboard = UIStoryboard.get(.initialisation)
+                UIApplication.shared.keyWindow?.rootViewController = storyboard.instantiateViewController(withIdentifier: "InitialisationViewController")
+                AuthenticationGuard.shared.hideLockWindow()
+            } catch {
+                fatalError()
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
--- keyn/Controllers/Requests/RequestViewController.swift
+++ keyn/Controllers/Requests/RequestViewController.swift
@@ -108,4 +108,5 @@ class RequestViewController: UIViewController {
         }
 
     }
+
 }
