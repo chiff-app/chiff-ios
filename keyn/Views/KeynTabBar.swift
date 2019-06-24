35a9d2d0fe6f9c9f4c30f778db82f18df11b623c
bas-d <7903735+bas-d@users.noreply.github.com>
2019-06-24 15:58:42 +0200

    Move views to views folder.

---
+++ keyn/Views/KeynTabBar.swift
@@ -0,0 +1,46 @@
+/*
+* Copyright © 2019 Keyn B.V.
+* All rights reserved.
+*/
+import UIKit
+
+class KeynTabBar: UITabBar {
+
+    let height: CGFloat = 90
+    var gradientView: UIView!
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard let window = UIApplication.shared.keyWindow else {
+            return super.sizeThatFits(size)
+        }
+        var sizeThatFits = super.sizeThatFits(size)
+        if #available(iOS 11.0, *) {
+            sizeThatFits.height = height + window.safeAreaInsets.bottom
+        } else {
+            sizeThatFits.height = height
+        }
+        return sizeThatFits
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if gradientView == nil {
+            let frame = CGRect(x: self.bounds.minX, y: self.bounds.minY - 60.0, width: self.bounds.width, height: 150.0)
+            gradientView = UIView(frame: frame)
+            self.insertSubview(gradientView, at: 0)
+        }
+
+        addBackgroundLayer()
+    }
+
+    private func addBackgroundLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = gradientView.bounds
+        var colors = [CGColor]()
+        colors.append(UIColor.primaryVeryLight.withAlphaComponent(0).cgColor)
+        colors.append(UIColor.primaryVeryLight.withAlphaComponent(1).cgColor)
+        gradientLayer.locations = [NSNumber(value: 0.0),NSNumber(value: 0.6)]
+        gradientLayer.colors = colors
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
