e89f3965fe6abf71f5c270bf140690aa6c059240
bas-d <7903735+bas-d@users.noreply.github.com>
2019-07-26 10:24:08 +0200

    Upgrade page layout

--- keyn/Controllers/Settings/ProductCollectionViewCell.swift
+++ keyn/Controllers/Settings/ProductCollectionViewCell.swift
@@ -13,26 +13,33 @@ class ProductCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var savingLabel: RoundLabel!
+    @IBOutlet weak var boxView: UIView!
+    @IBOutlet weak var radioButton: RadioButton!
 
     var isFirst: Bool = true {
         didSet {
             if isFirst {
-                layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+                savingLabel.isHidden = true
+                boxView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
             } else {
-                layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+                savingLabel.isHidden = false
+                boxView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
             }
         }
     }
 
     func showSelected() {
         layer.zPosition = isSelected ? 10 : 5
-        layer.borderColor = isSelected ? UIColor.primary.cgColor : UIColor.primaryTransparant.cgColor
+        boxView.layer.borderColor = isSelected ? UIColor.primary.cgColor : UIColor.primaryTransparant.cgColor
+        radioButton.enabled = isSelected
     }
 
     override func awakeFromNib() {
-        layer.borderColor = UIColor.primaryTransparant.cgColor
-        layer.borderWidth = 1.0
-        layer.cornerRadius = 6.0
+        super.awakeFromNib()
+        boxView.layer.borderColor = UIColor.primaryTransparant.cgColor
+        boxView.layer.borderWidth = 1.0
+        boxView.layer.cornerRadius = 6.0
+        savingLabel.isHidden = isFirst
     }
 
 }
@@ -40,7 +47,8 @@ class ProductCollectionViewCell: UICollectionViewCell {
 class RoundLabel: UILabel {
 
     override func awakeFromNib() {
-        layer.zPosition = CGFloat(INT_MAX)
+        layer.masksToBounds = true
+        layer.zPosition = 15
         layer.cornerRadius = 2.0
     }
 
