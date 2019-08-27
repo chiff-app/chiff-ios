a0585a5ec5a350289ee5eaf54a95f642a771c5b5
bas-d <7903735+bas-d@users.noreply.github.com>
2019-08-27 15:13:14 +0200

    Render privacy policy and terms from markdown

--- keyn/Controllers/Settings/WebViewController.swift
+++ keyn/Controllers/Settings/WebViewController.swift
@@ -4,11 +4,13 @@
  */
 import UIKit
 import WebKit
+import Down
 
 class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var loadingActivity: UIActivityIndicatorView!
+    var webView: WKWebView!
 
     var url: URL!
     var presentedModally = false
@@ -18,10 +20,33 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         if presentedModally {
             navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(cancel))
         }
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        let request = URLRequest(url: url)
-        webView.load(request)
+
+        if url.pathExtension.lowercased() == "markdown" || url.pathExtension.lowercased() == "md" {
+            do {
+                let bundleUrl = Bundle.main.path(forResource: "WebView", ofType: "bundle")
+                webView = try DownView(frame: containerView.frame, markdownString: String(contentsOf: url, encoding: .utf8), templateBundle: Bundle(url: URL(fileURLWithPath: bundleUrl!))) {
+                    self.loadingActivity.stopAnimating()
+                }
+            } catch {
+                loadingActivity.stopAnimating()
+                Logger.shared.error("Error paring markdown")
+                showError(message: "Error parsing data")
+            }
+        } else {
+            webView = WKWebView(frame: containerView.frame)
+            webView.uiDelegate = self
+            webView.navigationDelegate = self
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(webView)
+
+        webView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        webView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
