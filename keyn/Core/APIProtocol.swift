9c138deb921be69cb0fabce5c7bb90fefd773621
BrandonMA <maldonado.brandon177@gmail.com>
2019-09-04 23:21:13 -0500

    Created MockAPI

--- keyn/Core/APIProtocol.swift
+++ keyn/Core/APIProtocol.swift
@@ -76,29 +76,73 @@ protocol APIProtocol {
 
 }
 
+enum MockAPIError: Error {
+    case notImplemented
+    case noData
+    case noPublicKey
+    case badPublicKey
+}
+
 // For example
 class MockAPI: APIProtocol {
+    var mockData = [String: JSONObject]()
+    
+    init(pubKey: String? = nil, account: [String: String]? = nil) {
+        if let pubKey = pubKey, let account = account {
+            mockData[pubKey] = account
+        }
+    }
 
     func signedRequest(endpoint: APIEndpoint, method: APIMethod, message: JSONObject?, pubKey: String?, privKey: Data, body: Data? = nil, completionHandler: @escaping (Result<JSONObject, Error>) -> Void) {
-        // TODO: Implement
+        switch endpoint {
+        case .backup: completionHandler(backupCall(method: method, message: message, pubKey: pubKey, privKey: privKey, body: body))
+        default: completionHandler(.failure(MockAPIError.notImplemented))
+        }
     }
 
     func request(endpoint: APIEndpoint, path: String?, parameters: RequestParameters, method: APIMethod, signature: String?, body: Data? = nil, completionHandler: @escaping (Result<JSONObject, Error>) -> Void) {
         // TODO
     }
 
-}
-
-// Testing:
-
-class SomeTest {
-
-    func setUp() {
-        API.shared = MockAPI()
+    private func backupCall(method: APIMethod, message: JSONObject?, pubKey: String?, privKey: Data, body: Data?) -> Result<JSONObject, Error> {
+        guard let pubKey = pubKey else {
+            return .failure(MockAPIError.noPublicKey)
+        }
+        switch method {
+        case .get:
+            guard let data = mockData[pubKey] else {
+                return .failure(MockAPIError.noData)
+            }
+            return .success(data)
+        case .delete:
+            if let message = message, let id = message["id"] as? String {
+                if mockData[pubKey]?.removeValue(forKey: id) != nil {
+                    return .success(JSONObject())
+                } else {
+                    return .failure(MockAPIError.badPublicKey)
+                }
+            } else if pubKey.contains("/all") {
+                let newPubKey = String(pubKey[pubKey.startIndex..<pubKey.index(pubKey.endIndex, offsetBy: -4)])
+                if mockData.removeValue(forKey: String(newPubKey)) != nil {
+                    return .success(JSONObject())
+                } else {
+                    return .failure(MockAPIError.badPublicKey)
+                }
+            }
+            return .success(JSONObject())
+        case .post:
+            guard let message = message, let id = message["id"] as? String, let recoverData = message["data"] as? String else {
+                return .failure(MockAPIError.noData)
+            }
+            mockData[pubKey] = [id: recoverData]
+            return .success(JSONObject())
+        case .put:
+            if let _ = mockData[pubKey] { // Check if the object exists so it updates, if not then it should fail
+                mockData[pubKey] = message
+                return .success(JSONObject())
+            } else {
+                return .failure(MockAPIError.noData)
+            }
+        }
     }
-
-    func testblabla() {
-        // Any function calls that to the API will now execute the MockAPI implementation functions.
-    }
 }
-
