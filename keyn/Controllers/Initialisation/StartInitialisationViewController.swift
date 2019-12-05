14f81c5a9db540987d62353be578d0fb2fcc3064
bas-d <7903735+bas-d@users.noreply.github.com>
2019-12-05 17:18:38 +0100

    Update onboarding process

---
+++ keyn/Controllers/Initialisation/StartInitialisationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StartInitialisationViewController.swift
+//  keyn
+//
+//  Created by Bas Doorn on 05/12/2019.
+//  Copyright © 2019 keyn. All rights reserved.
+//
+
+import UIKit
+
+class StartInitialisationViewController: UIViewController {
+
+    private let showTermsSegue = "ShowTerms"
+    private let startOnboardingSegue = "StartOnboarding"
+
+    @IBAction func showTerms(_ sender: UIButton) {
+        performSegue(withIdentifier: Properties.agreedWithTerms ? startOnboardingSegue : showTermsSegue, sender: self)
+    }
+
+    @IBAction func unwindAndStartOnboarding(sender: UIStoryboardSegue) {
+        Properties.agreedWithTerms = true
+        performSegue(withIdentifier: startOnboardingSegue, sender: self)
+    }
+
+}
