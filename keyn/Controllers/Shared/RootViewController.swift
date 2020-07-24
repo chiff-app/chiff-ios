630285cc152c7194f15d21da57947a163215d921
bas-d <7903735+bas-d@users.noreply.github.com>
2020-07-24 10:54:55 +0200

    Update terms of use + notify new users

--- keyn/Controllers/Shared/RootViewController.swift
+++ keyn/Controllers/Shared/RootViewController.swift
@@ -16,12 +16,33 @@ class RootViewController: UITabBarController {
         tabBar.items?[2].title = "tabs.settings".localized
         tabBar.unselectedItemTintColor = UIColor.primaryHalfOpacity
         tabBar.tintColor = UIColor.primary
+        launchTerms()
     }
 
     func setBadge(completed: Bool) {
         if let settingsItem = tabBar.items?[2] {
             settingsItem.badgeColor = UIColor.secondary
             settingsItem.badgeValue = !completed || Properties.isJailbroken ? "!" : nil
+        }
+    }
+
+    func launchTerms() {
+        if Properties.notifiedLatestTerms {
+            return
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let alert = UIAlertController(title: "popups.questions.terms".localized, message: "popups.questions.updated_terms_message".localized, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "popups.responses.dont_care".localized, style: .cancel) { action in
+                Properties.notifiedLatestTerms = true
+            })
+            let agreeAction = UIAlertAction(title: "popups.responses.open".localized, style: .default, handler: { action in
+                let urlPath = Bundle.main.path(forResource: "terms_of_use", ofType: "md")
+                self.performSegue(withIdentifier: "ShowTerms", sender: URL(fileURLWithPath: urlPath!))
+                Properties.notifiedLatestTerms = true
+            })
+            alert.addAction(agreeAction)
+            alert.preferredAction = agreeAction
+            self.present(alert, animated: true, completion: nil)
         }
     }
 
@@ -32,10 +53,15 @@ class RootViewController: UITabBarController {
         }
     }
 
-//    func showGradient(_ value: Bool) {
-//        (tabBar as! KeynTabBar).gradientView.isHidden = !value
-//    }
-    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination.contents as? WebViewController, let url = sender as? URL {
+            destination.presentedModally = true
+            destination.url = url
+        }
+    }
+
     // MARK: - Private functions
 
     private func presentQuestionAlert(questionnaire: Questionnaire) {
