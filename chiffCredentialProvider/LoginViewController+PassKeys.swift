e2085458bcb4dc2827c97160a3b6783d209d60c8
bas-d <7903735+bas-d@users.noreply.github.com>
2023-11-03 15:09:12 +0100

    Get passkeys working.

---
+++ chiffCredentialProvider/LoginViewController+PassKeys.swift
@@ -0,0 +1,119 @@
+//
+//  LoginViewController+PassKeys.swift
+//  chiffCredentialProvider
+//
+//  Copyright: see LICENSE.md
+//
+
+import UIKit
+import AuthenticationServices
+import LocalAuthentication
+import ChiffCore
+
+@available(iOS 17.0, *)
+extension LoginViewController {
+
+    override func prepareInterface(forPasskeyRegistration registrationRequest: ASCredentialRequest) {
+        guard let request = registrationRequest as? ASPasskeyCredentialRequest,
+              let credentialIdentity = request.credentialIdentity as? ASPasskeyCredentialIdentity else {
+            return
+        }
+        self.type = .passkeyRegistration
+        let algorithms = request.supportedAlgorithms.compactMap { WebAuthnAlgorithm(rawValue: $0.rawValue) }
+        self.passkeyRegistrationRequest = PasskeyRegistrationRequest(clientDataHash: request.clientDataHash,
+                                                                     relyingPartyIdentifier: credentialIdentity.relyingPartyIdentifier,
+                                                                     userHandle: credentialIdentity.userHandle,
+                                                                     userName: credentialIdentity.userName,
+                                                                     serviceIdentifier: credentialIdentity.serviceIdentifier,
+                                                                     algorithms: algorithms
+        )
+    }
+
+    override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier], requestParameters: ASPasskeyCredentialRequestParameters) {
+        self.serviceIdentifiers = serviceIdentifiers
+        self.type = .passkeyAssertion
+        self.passkeyAssertionRequest = PasskeyAssertionRequest(
+            clientDataHash: requestParameters.clientDataHash,
+            relyingPartyIdentifier: requestParameters.relyingPartyIdentifier
+        )
+    }
+
+    override func prepareInterfaceToProvideCredential(for credentialRequest: ASCredentialRequest) {
+        self.username = credentialRequest.credentialIdentity.user
+        self.accountId = credentialRequest.credentialIdentity.recordIdentifier
+        switch credentialRequest.type {
+        case .passkeyAssertion:
+            guard let request = credentialRequest as? ASPasskeyCredentialRequest,
+                  let credentialIdentity = request.credentialIdentity as? ASPasskeyCredentialIdentity else {
+                return
+            }
+            self.passkeyAssertionRequest = PasskeyAssertionRequest(clientDataHash: request.clientDataHash, relyingPartyIdentifier: credentialIdentity.relyingPartyIdentifier)
+        default:
+            print("TODO")
+        }
+    }
+
+    override func provideCredentialWithoutUserInteraction(for credentialRequest: ASCredentialRequest) {
+        do {
+            switch credentialRequest.type {
+            case .passkeyAssertion:
+                guard let request = credentialRequest as? ASPasskeyCredentialRequest, let credentialIdentity = request.credentialIdentity as? ASPasskeyCredentialIdentity else {
+                    throw NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue)
+                }
+                guard let account = try UserAccount.getAny(id: credentialIdentity.credentialID.hexEncodedString(), context: nil) as? UserAccount else {
+                    return self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.credentialIdentityNotFound.rawValue))
+                }
+                let assertionRequest = PasskeyAssertionRequest(clientDataHash: request.clientDataHash, relyingPartyIdentifier: credentialIdentity.relyingPartyIdentifier)
+                try completeWebauthnAssertion(with: assertionRequest, account: account, context: nil)
+            case .password:
+                guard let account = try UserAccount.getAny(id: credentialRequest.credentialIdentity.recordIdentifier!, context: nil) else {
+                    return self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.credentialIdentityNotFound.rawValue))
+                }
+                guard let password = try account.password(context: nil) else {
+                    return self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.credentialIdentityNotFound.rawValue))
+                }
+                let passwordCredential = ASPasswordCredential(user: account.username, password: password)
+                self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
+            @unknown default:
+                throw NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue)
+            }
+        } catch KeychainError.interactionNotAllowed {
+            self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.userInteractionRequired.rawValue))
+        } catch {
+            self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue))
+        }
+    }
+
+    func completeWebauthnAssertion(with assertionRequest: PasskeyAssertionRequest, account: UserAccount, context: LAContext?) throws {
+        let signature = try account.webAuthnSign(challenge: assertionRequest.clientDataHash, rpId: assertionRequest.relyingPartyIdentifier)
+        let credential = ASPasskeyAssertionCredential(userHandle: account.webAuthn!.userHandle?.data ?? Data(),
+                                                      relyingParty: account.webAuthn!.id,
+                                                      signature: signature,
+                                                      clientDataHash: assertionRequest.clientDataHash,
+                                                      authenticatorData: account.webAuthn!.authenticatorData,
+                                                      credentialID: account.id.fromHex!)
+        self.extensionContext.completeAssertionRequest(using: credential)
+    }
+
+    func completePasskeyRegistration(with registrationRequest: PasskeyRegistrationRequest, context: LAContext?) throws {
+        guard let site = registrationRequest.serviceIdentifier.site else {
+            throw AutofillError.invalidURL
+        }
+        let webauthn = try WebAuthn(id: registrationRequest.relyingPartyIdentifier,
+                                    algorithms: registrationRequest.algorithms,
+                                    userHandle: String(data: registrationRequest.userHandle, encoding: .utf8))
+        let account = try UserAccount(username: registrationRequest.userName,
+                                      sites: [site],
+                                      password: nil,
+                                      webauthn: webauthn,
+                                      notes: nil,
+                                      askToChange: false,
+                                      context: context)
+        let attestationObject = try webauthn.getAttestation(accountId: account.id, extensions: nil)
+        let credential = ASPasskeyRegistrationCredential(relyingParty: webauthn.id,
+                                                         clientDataHash: registrationRequest.clientDataHash,
+                                                         credentialID: account.id.fromHex!,
+                                                         attestationObject: attestationObject)
+        self.extensionContext.completeRegistrationRequest(using: credential)
+    }
+}
--- chiffCredentialProvider/LoginViewController.swift
+++ chiffCredentialProvider/LoginViewController.swift
@@ -11,16 +11,29 @@ import AuthenticationServices
 import PromiseKit
 import ChiffCore
 
+enum AutofillError: Error {
+    case invalidURL
+}
+
+enum AutoFillRequestType {
+    case passkeyAssertion
+    case passkeyRegistration
+    case passwordLogin
+    case passwordRegistration
+}
+
 class LoginViewController: ASCredentialProviderViewController {
     @IBOutlet weak var touchIDButton: UIButton!
     @IBOutlet weak var requestLabel: UILabel!
     var credentialProviderViewController: CredentialProviderViewController?
     var shouldAsk: Bool = false
     var serviceIdentifiers: [ASCredentialServiceIdentifier]!
+    var type: AutoFillRequestType!
     var accounts: [String: Account]!
     var accountId: String?
     var username: String?
-    var clientDataHash: Data?
+    var passkeyRegistrationRequest: PasskeyRegistrationRequest?
+    var passkeyAssertionRequest: PasskeyAssertionRequest?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +45,14 @@ class LoginViewController: ASCredentialProviderViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if let username = self.username {
-            requestLabel.text = String(format: "requests.login_with".localized, username)
-        } else {
+        switch type {
+        case .passkeyRegistration:
+            requestLabel.text = String(format: "requests.add_site".localized, passkeyRegistrationRequest!.relyingPartyIdentifier)
+        case .passkeyAssertion:
+            requestLabel.text = String(format: "requests.login_to".localized, passkeyAssertionRequest!.relyingPartyIdentifier)
+        case .passwordLogin:
+            requestLabel.text = String(format: "requests.login_with".localized, username!)
+        default:
             requestLabel.text = "requests.unlock_accounts".localized
         }
     }
@@ -74,84 +92,45 @@ class LoginViewController: ASCredentialProviderViewController {
         firstly {
             LocalAuthenticationManager.shared.authenticate(reason: reason, withMainContext: true)
         }.done { context in
-            guard let accountID = self.accountId, let account = try UserAccount.getAny(id: accountID, context: context) else {
-                let accounts = try UserAccount.allCombined(context: context)
-                DispatchQueue.main.async {
-                    if accounts.isEmpty {
-                        self.performSegue(withIdentifier: "ShowAddAccount", sender: self)
-                    } else {
+            switch self.type {
+            case .passkeyRegistration:
+                guard #available(iOS 17.0, *), let passkeyRegistrationRequest = self.passkeyRegistrationRequest else {
+                    return self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue))
+                }
+                try self.completePasskeyRegistration(with: passkeyRegistrationRequest, context: context)
+            case .passkeyAssertion:
+                guard #available(iOS 17.0, *), let passkeyAssertionRequest = self.passkeyAssertionRequest else {
+                    return self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue))
+                }
+                guard let accountID = self.accountId, let account = try UserAccount.getAny(id: accountID, context: context) as? UserAccount else {
+                    let accounts = try UserAccount.all(context: context).filter { $1.webAuthn != nil }
+                    DispatchQueue.main.async {
                         self.accounts = accounts
                         self.performSegue(withIdentifier: "ShowAccounts", sender: self)
                     }
+                    return
                 }
-                return
-            }
-            if #available(iOS 17.0, *), let clientDataHash = self.clientDataHash, let userAccount = account as? UserAccount {
-                try self.completeWebauthnAssertion(context: context, clientDataHash: clientDataHash, account: userAccount)
-            } else if let password = try account.password(context: context) {
-                let passwordCredential = ASPasswordCredential(user: account.username, password: password)
-                self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
-            } else {
+                try self.completeWebauthnAssertion(with: passkeyAssertionRequest, account: account, context: context)
+            case .passwordLogin:
+                guard let accountID = self.accountId, let account = try UserAccount.getAny(id: accountID, context: context) else {
+                    let accounts = try UserAccount.allCombined(context: context)
+                    DispatchQueue.main.async {
+                        if accounts.isEmpty {
+                            self.performSegue(withIdentifier: "ShowAddAccount", sender: self)
+                        } else {
+                            self.accounts = accounts
+                            self.performSegue(withIdentifier: "ShowAccounts", sender: self)
+                        }
+                    }
+                    return
+                }
+                try self.completePasswordLoginRequest(account: account, context: context)
+            default:
                 self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.credentialIdentityNotFound.rawValue))
             }
         }.catch { _ in
             self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue))
         }
     }
-}
 
-
-extension LoginViewController {
-    
-    override func prepareInterfaceToProvideCredential(for credentialIdentity: ASPasswordCredentialIdentity) {
-        self.username = credentialIdentity.user
-        self.accountId = credentialIdentity.recordIdentifier
-    }
-
-    override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
-        self.serviceIdentifiers = serviceIdentifiers
-    }
-    
-    override func provideCredentialWithoutUserInteraction(for credentialIdentity: ASPasswordCredentialIdentity) {
-        do {
-            guard let account = try UserAccount.getAny(id: credentialIdentity.recordIdentifier!, context: nil), let password = try account.password(context: nil) else {
-                return self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.credentialIdentityNotFound.rawValue))
-            }
-
-            let passwordCredential = ASPasswordCredential(user: account.username, password: password)
-            self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
-        } catch KeychainError.interactionNotAllowed {
-            self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.userInteractionRequired.rawValue))
-        } catch {
-            self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.failed.rawValue))
-        }
-    }
-}
-
-@available(iOSApplicationExtension 17.0, *)
-extension LoginViewController {
-    
-//    override func prepareInterface(forPasskeyRegistration registrationRequest: ASCredentialRequest) {
-//        
-//    }
-//    
-    override func prepareInterfaceToProvideCredential(for credentialRequest: ASCredentialRequest) {
-        self.username = credentialRequest.credentialIdentity.user
-        self.accountId = credentialRequest.credentialIdentity.recordIdentifier
-        print(credentialRequest.credentialIdentity.serviceIdentifier.identifier)
-        if credentialRequest.type == .passkeyAssertion, let request = credentialRequest as? ASPasskeyCredentialRequest {
-            self.clientDataHash = request.clientDataHash
-        }
-    }
-    
-//    override func provideCredentialWithoutUserInteraction(for credentialRequest: ASCredentialRequest) {
-//        <#code#>
-//    }
-    
-    func completeWebauthnAssertion(context: LAContext, clientDataHash: Data, account: UserAccount) throws {
-        // TODO, check if we can use the serviceIdentifier for this..
-        let signature = try account.webAuthnSign(challenge: clientDataHash, rpId: account.webAuthn!.id)
-        let credential = ASPasskeyAssertionCredential(userHandle: account.webAuthn!.userHandle?.data ?? Data(), relyingParty: account.webAuthn!.id, signature: signature, clientDataHash: clientDataHash, authenticatorData: account.webAuthn!.authenticatorData, credentialID: account.id.fromHex!)
-        self.extensionContext.completeAssertionRequest(using: credential)
-    }
 }
