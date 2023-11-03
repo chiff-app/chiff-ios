c5fbf8fe40727cbe24122670cad06a08e0b0dc91
bas-d <7903735+bas-d@users.noreply.github.com>
2023-11-03 14:32:05 +0100

    Also store passkeys in identity store.

--- ChiffCore/Sources/ChiffCore/Core/Keychain+Migration.swift
+++ ChiffCore/Sources/ChiffCore/Core/Keychain+Migration.swift
@@ -36,6 +36,7 @@ extension Keychain {
                 Properties.currentKeychainVersion = 2
             case let n where n < 3:
                 try migrateKeychainGroup(id: KeyIdentifier.webauthn.identifier(for: .webAuthnSeed), service: .webAuthnSeed, oldGroup: "35MFYY2JY5.io.keyn.keyn", context: context)
+                Properties.reloadAccounts = true
                 Properties.currentKeychainVersion = 3
             default:
                 return
--- ChiffCore/Sources/ChiffCore/Models/Account/Account+AuthenticationServices.swift
+++ ChiffCore/Sources/ChiffCore/Models/Account/Account+AuthenticationServices.swift
@@ -13,18 +13,21 @@ extension Account {
     /// Save this account to the IdentityStore, so it is available for AutoFill.
     func saveToIdentityStore() {
         ASCredentialIdentityStore.shared.getState { (state) in
-            if !state.isEnabled {
+            guard state.isEnabled else {
                 return
-            } else if state.supportsIncrementalUpdates {
+            }
+            guard state.supportsIncrementalUpdates else {
+                Self.saveCredentialIdentities()
+                return
+            }
+            if #available(iOS 17.0, *), let webAuthn = (self as? UserAccount)?.webAuthn {
+                let identity = ASPasskeyCredentialIdentity(relyingPartyIdentifier: webAuthn.id, userName: self.username, credentialID: self.id.fromHex!, userHandle: webAuthn.userHandle?.data ?? Data())
+                ASCredentialIdentityStore.shared.saveCredentialIdentities([identity], completion: nil)
+            }
+            if hasPassword {
                 let service = ASCredentialServiceIdentifier(identifier: self.site.url, type: ASCredentialServiceIdentifier.IdentifierType.URL)
                 let identity = ASPasswordCredentialIdentity(serviceIdentifier: service, user: self.username, recordIdentifier: self.id)
                 ASCredentialIdentityStore.shared.saveCredentialIdentities([identity], completion: nil)
-            } else if let accounts = try? Self.all(context: nil) {
-                let identities = accounts.values.map { (account) -> ASPasswordCredentialIdentity in
-                    let service = ASCredentialServiceIdentifier(identifier: account.site.url, type: ASCredentialServiceIdentifier.IdentifierType.URL)
-                    return ASPasswordCredentialIdentity(serviceIdentifier: service, user: account.username, recordIdentifier: account.id)
-                }
-                ASCredentialIdentityStore.shared.saveCredentialIdentities(identities, completion: nil)
             }
         }
     }
@@ -32,14 +35,21 @@ extension Account {
     /// Delete this account from the IdentityStore, so it is no longer available for Autofill
     func deleteFromToIdentityStore() {
         ASCredentialIdentityStore.shared.getState { (state) in
-            if !state.isEnabled {
+            guard state.isEnabled else {
                 return
-            } else if state.supportsIncrementalUpdates {
+            }
+            guard state.supportsIncrementalUpdates else {
+                Self.saveCredentialIdentities()
+                return
+            }
+            if #available(iOS 17.0, *), let webAuthn = (self as? UserAccount)?.webAuthn {
+                let identity = ASPasskeyCredentialIdentity(relyingPartyIdentifier: webAuthn.id, userName: self.username, credentialID: self.id.fromHex!, userHandle: webAuthn.userHandle?.data ?? Data())
+                ASCredentialIdentityStore.shared.removeCredentialIdentities([identity], completion: nil)
+            }
+            if hasPassword {
                 let service = ASCredentialServiceIdentifier(identifier: self.site.url, type: ASCredentialServiceIdentifier.IdentifierType.URL)
                 let identity = ASPasswordCredentialIdentity(serviceIdentifier: service, user: self.username, recordIdentifier: self.id)
                 ASCredentialIdentityStore.shared.removeCredentialIdentities([identity], completion: nil)
-            } else {
-                Self.reloadIdentityStore()
             }
         }
     }
@@ -49,13 +59,38 @@ extension Account {
         ASCredentialIdentityStore.shared.removeAllCredentialIdentities({ (result, error) in
             if let error = error {
                 Logger.shared.error("Error deleting credentials from identity store", error: error)
-            } else if result, let accounts = try? Self.all(context: nil) {
-                let identities = accounts.values.map { (account) -> ASPasswordCredentialIdentity in
-                    let service = ASCredentialServiceIdentifier(identifier: account.site.url, type: ASCredentialServiceIdentifier.IdentifierType.URL)
-                    return ASPasswordCredentialIdentity(serviceIdentifier: service, user: account.username, recordIdentifier: account.id)
-                }
-                ASCredentialIdentityStore.shared.saveCredentialIdentities(identities, completion: nil)
+            } else if result {
+                saveCredentialIdentities()
             }
         })
     }
+    
+    private static func saveCredentialIdentities() {
+        guard let accounts = try? Self.all(context: nil) else {
+            return
+        }
+        if #available(iOS 17.0, *) {
+            var identities = [ASCredentialIdentity]()
+            for account in accounts.values {
+                if let webAuthn = (account as? UserAccount)?.webAuthn {
+                    identities.append(ASPasskeyCredentialIdentity(relyingPartyIdentifier: webAuthn.id, userName: account.username, credentialID: account.id.fromHex!, userHandle: webAuthn.userHandle?.data ?? Data()))
+                }
+                if account.hasPassword {
+                    let service = ASCredentialServiceIdentifier(identifier: account.site.url, type: ASCredentialServiceIdentifier.IdentifierType.URL)
+                    identities.append(ASPasswordCredentialIdentity(serviceIdentifier: service, user: account.username, recordIdentifier: account.id))
+                }
+            }
+            ASCredentialIdentityStore.shared.saveCredentialIdentities(identities, completion: nil)
+        } else {
+            let identities = accounts.values.compactMap { (account) -> ASPasswordCredentialIdentity? in
+                guard account.hasPassword else {
+                    return nil
+                }
+                let service = ASCredentialServiceIdentifier(identifier: account.site.url, type: ASCredentialServiceIdentifier.IdentifierType.URL)
+                return ASPasswordCredentialIdentity(serviceIdentifier: service, user: account.username, recordIdentifier: account.id)
+            }
+            ASCredentialIdentityStore.shared.saveCredentialIdentities(identities, completion: nil)
+        }
+    }
+    
 }
